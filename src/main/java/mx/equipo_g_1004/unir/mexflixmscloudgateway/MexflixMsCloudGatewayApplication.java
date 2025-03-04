b1c63abf3cb5bdbae317a1724d3be32e0a98a420
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-03 23:31:35 -0600

    initialize mexflix-ms-cloud-gateway project with Spring Boot setup

---
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmscloudgateway/MexflixMsCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexflixMsCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MexflixMsCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/equipo_g_1004/unir/mexflixmscloudgateway/MexflixMsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.equipo_g_1004.unir.mexflixmscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexflixMsCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

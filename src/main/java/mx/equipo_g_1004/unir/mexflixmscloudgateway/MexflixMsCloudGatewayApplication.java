5394fbc4174bd7ace0e3523d243ac02c8f24073c
AgnosticSix <mateo_godfather@hotmail.com>
2025-03-04 02:00:31 -0600

    refactor configuration: rename application.properties to application.yml and update Spring Cloud Gateway settings

--- src/main/java/mx/equipo_g_1004/unir/mexflixmscloudgateway/MexflixMsCloudGatewayApplication.java
+++ src/main/java/mx/equipo_g_1004/unir/mexflixmscloudgateway/MexflixMsCloudGatewayApplication.java
@@ -2,8 +2,10 @@ package mx.equipo_g_1004.unir.mexflixmscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MexflixMsCloudGatewayApplication {
 
     public static void main(String[] args) {
